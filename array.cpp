37c56b45d325234ef338cf86b5d55919b777f266
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-07 17:37:21 +0700

    menampilkan data dengan perintah satu persatu

--- array.cpp
+++ array.cpp
@@ -25,4 +25,12 @@ int main()
     cin >> datamu [3];
     cout << "Isikan data kelima : ";
     cin >> datamu [4];
+    cout << endl;
+
+    //Menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
 }
